f1529d7aff9976ddbf34e5057f0a0aea765adb1f
YuukiYamano <yuukiyamano1108@gmail.com>
2026-01-06 13:48:19 +0900

    reset

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 def main():
     while True:
         print("選択してください：")
-        print("1: ")
-        print("2: ")
-        print("3: ")
+        print("1: 選択肢1")
+        print("2: 選択肢2")
+        print("3: 選択肢2")
         print("q: 終了")
 
         choice = input("> ")
 
         if choice == "1":
-            print("が選ばれました。")
+            print("選択肢1が選ばれました。")
         elif choice == "2":
-            print("が選ばれました。")
+            print("選択肢2が選ばれました。")
         elif choice == "3":
-            print("が選ばれました。")
+            print("選択肢3が選ばれました。")
         elif choice == "q":
             print("プログラムを終了します。")
             break
