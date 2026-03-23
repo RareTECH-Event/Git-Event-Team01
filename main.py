fafb9cdff30a012f6bdbfd342eea9bb4087a677b
sakurasou3 <ko.ki.0216@gmail.com>
2026-03-23 21:52:11 +0900

    選択肢4を追加

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
         print("1: 選択肢1")
         print("2: 選択肢2")
         print("3: 選択肢3")
+        print("4: 選択肢4")
         print("q: 終了")
 
         choice = input("> ")
@@ -14,11 +15,14 @@ def main():
             print("選択肢2が選ばれました。")
         elif choice == "3":
             print("選択肢3が選ばれました。")
+        elif choice == "4":
+            print("選択肢4が選ばれました。")
         elif choice == "q":
             print("プログラムを終了します。")
             break
         else:
             print("無効な入力です。もう一度選択してください。")
+
 
 if __name__ == "__main__":
     main()
