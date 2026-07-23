8a88cc059cbafd5959f6f90903f534b974d5aa5f
shinwa <yorozustudy800@gmail.com>
2026-07-23 22:06:01 +0900

    選択肢3を「しんわ@68期」に変更

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ def main():
         print("選択してください：")
         print("1: まさき@65期")
         print("2: さえ@66期")
-        print("3: 選択肢3")
+        print("3: しんわ@68期")
         print("q: 終了")
 
         choice = input("> ")
 
         if choice == "1":
-            print("選択肢1が選ばれました。")
+            print("まさきが選ばれました。")
         elif choice == "2":
             print("さえが選ばれました。")
         elif choice == "3":
-            print("選択肢3が選ばれました。")
+            print("しんわが選ばれました。")
         elif choice == "q":
             print("プログラムを終了します。")
             break
